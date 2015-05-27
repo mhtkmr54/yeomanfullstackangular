17219dd6491d2dc13a17303aa19e5f2cae42e6cd
Mohit Kumar <mhtkmr54@gmail.com>
2015-05-27 23:44:47 +0530

    basic angular fullstack

---
+++ client/app/admin/admin.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('angularTubeApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/admin', {
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
